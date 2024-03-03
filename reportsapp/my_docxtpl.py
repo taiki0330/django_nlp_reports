61411fcc921803196078ebd55d9c8ffacb2f10d6
taiki0330 <jonas.taiki.0330@icloud.com>
2024-03-04 07:26:00 +0900

    docxtpl example date

--- reportsapp/my_docxtpl.py
+++ reportsapp/my_docxtpl.py
@@ -1,6 +1,15 @@
 from docxtpl import DocxTemplate
 
-doc = DocxTemplate("./docx_templates/example.docx")
-context = { 'title' : "ワード" }
+doc = DocxTemplate("/Users/matsuzakidaiki/anaconda3/envs/django_nlp_env/reportsproject/reportsapp/docx_templates/Paragraph_usage_List2JP.docx")
+context = { 
+           'create_date' : "令和６年３月４日",
+           'title': '被疑者の人相着衣',
+           'crime_date': '令和６年３月３日',
+           'crime_name': '暴行',
+           'photograph_date': '令和６年３月４日',
+           'photograph_place': '福岡県東警察署',
+           'suspect_honseki': '福岡市中央区大名一丁目１番地',
+           'suspect_address': ''
+           }
 doc.render(context)
 doc.save("generated_doc.docx")
